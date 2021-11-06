e82bfc58b5ababcd0e29ea333c39bfb5851fca37
ujin-Bang <qkddnwls14@gmail.com>
2021-11-06 16:55:55 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/fragment_20211106/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.techtown.fragment_20211106
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
