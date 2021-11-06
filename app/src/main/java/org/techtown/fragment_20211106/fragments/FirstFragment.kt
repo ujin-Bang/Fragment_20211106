1c2bd263be46aa0c840ac395706ebfc98eadf4cc
ujin-Bang <qkddnwls14@gmail.com>
2021-11-06 17:51:56 +0900

    프래그먼트 동작 코드   -화면 전달 :requireContext()활용

--- app/src/main/java/org/techtown/fragment_20211106/fragments/FirstFragment.kt
+++ app/src/main/java/org/techtown/fragment_20211106/fragments/FirstFragment.kt
@@ -1,10 +1,13 @@
 package org.techtown.fragment_20211106.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_firt.*
 import org.techtown.fragment_20211106.R
 
 class FirstFragment : Fragment() {
@@ -18,4 +21,23 @@ class FirstFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_firt,container,false)
 
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLog.setOnClickListener {
+            Log.d("첫번째프래그먼트","테스트로그")
+
+        }
+
+        btnToast.setOnClickListener {
+
+            Toast.makeText(requireContext(), "토스트띄우기", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+
+
 }
