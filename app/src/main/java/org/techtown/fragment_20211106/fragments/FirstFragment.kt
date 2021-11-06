682ab9fb9e5458236a63bf4b2d2f5b23be09f505
ujin-Bang <qkddnwls14@gmail.com>
2021-11-06 17:20:24 +0900

    프래그먼트 크래스 추가   - Fragment() 상속   - xml만 커스텀 -> onCreateView 오버라이딩

---
+++ app/src/main/java/org/techtown/fragment_20211106/fragments/FirstFragment.kt
@@ -0,0 +1,21 @@
+package org.techtown.fragment_20211106.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import org.techtown.fragment_20211106.R
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_firt,container,false)
+
+    }
+}
